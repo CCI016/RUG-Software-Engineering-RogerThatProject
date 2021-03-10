4667bbe24e3983085787232c396c4cda22bf7818
mavceanscaia <49808265+mavceanscaia@users.noreply.github.com>
2021-03-10 20:56:52 +0200

    contact component added, startpage changed

--- webApp/src/app/app-routing.module.ts
+++ webApp/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from "./pages/contact/contact.component";
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
+  { path: 'contact', component: ContactComponent}
 ];
 
 @NgModule({
--- webApp/src/app/app.module.ts
+++ webApp/src/app/app.module.ts
@@ -13,12 +13,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IconsProviderModule } from './icons-provider.module';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { ContactComponent } from './pages/contact/contact.component';
 
 registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
