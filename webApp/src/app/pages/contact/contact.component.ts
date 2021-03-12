7c3ab77d81104344d4b0fc17867c4da5d6cf550a
mavceanscaia <49808265+mavceanscaia@users.noreply.github.com>
2021-03-12 18:42:15 +0200

    contact form added

--- webApp/src/app/pages/contact/contact.component.ts
+++ webApp/src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  //items = this.contact.group();
+  contactForm = this.formBuilder.group({
+    name: '',
+    emailAddress: '',
+    message : ''
+  });
+  constructor(private formBuilder: FormBuilder,private contact: ContactComponent) { }
+  onSubmit(): void {
+    // Process checkout data here
+    //this.items = this.contact.clearAll();
+    console.warn('Your message has been sent', this.contactForm.value);
+    this.contactForm.reset();
+  }
+  ngOnInit():void  {
   }
 
 }
