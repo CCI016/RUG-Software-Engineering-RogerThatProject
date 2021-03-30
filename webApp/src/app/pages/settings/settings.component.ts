9a94bc8613265b5fc72c1ee3abd1d6f8c3d43167
Denis Garabajiu <denis1610@yahoo.com>
2021-03-30 15:36:46 +0300

    Phone number left

--- webApp/src/app/pages/settings/settings.component.ts
+++ webApp/src/app/pages/settings/settings.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { NzFormTooltipIcon } from 'ng-zorro-antd/form';
+
 
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
-})
+    selector: 'app-settings',
+    templateUrl: './settings.component.html',
+    styleUrls: ['./settings.component.css']
+  })
+
 export class SettingsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    validateForm!: FormGroup;
+    captchaTooltipIcon: NzFormTooltipIcon = {
+      type: 'info-circle',
+      theme: 'twotone'
+    };
+  
+    ngOnInit(): void;
+    
+    submitForm(): void {
+      for (const i in this.validateForm.controls) {
+        this.validateForm.controls[i].markAsDirty();
+        this.validateForm.controls[i].updateValueAndValidity();
+      }
+    }
+  
+    updateConfirmValidator(): void {
+      /** wait for refresh value */
+      Promise.resolve().then(() => this.validateForm.controls.checkPassword.updateValueAndValidity());
+    }
+  
+    confirmationValidator = (control: FormControl): { [s: string]: boolean } => {
+      if (!control.value) {
+        return { required: true };
+      } else if (control.value !== this.validateForm.controls.password.value) {
+        return { confirm: true, error: true };
+      }
+      return {};
+    }
 }
