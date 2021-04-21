fbb4ba9ca8d5c6dc92893c621794531224adeaa1
mavceanscaia <49808265+mavceanscaia@users.noreply.github.com>
2021-04-21 14:21:40 +0300

    other errors fixed

--- webApp/src/app/_helpers/error.interceptor.ts
+++ webApp/src/app/_helpers/error.interceptor.ts
@@ -3,7 +3,7 @@ import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/c
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-import { AuthenticationService } from '@app/_services';
+import { AuthenticationService } from '../_services/authentication.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
@@ -19,6 +19,6 @@ export class ErrorInterceptor implements HttpInterceptor {
 
             const error = err.error.message || err.statusText;
             return throwError(error);
-        }))
+        }));
     }
 }
