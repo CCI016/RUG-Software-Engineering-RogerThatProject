4c2dc620fed3892adda537526e21735fc9b558cd
Gasan Rzaev <42190535+grzaev@users.noreply.github.com>
2021-05-16 14:33:31 +0200

    Update AuthEndpoint.java
    register persist

--- src/main/java/org/rogerthat/endpoints/AuthEndpoint.java
+++ src/main/java/org/rogerthat/endpoints/AuthEndpoint.java
@@ -2,6 +2,7 @@ package org.rogerthat.endpoints;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
+import org.rogerthat.orm.User;
 
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
@@ -30,7 +31,7 @@ public class AuthEndpoint {
     @Path("/upload")
     @Consumes("multipart/form-data")
     @Transactional
-    public Response Login(String email, String pass) {
+    public Response Login(@QueryParam("email") String email,@QueryParam("password") String pass) {
         String usersPass = getPass(email);
 
         // Check if the strings are matching
@@ -49,20 +50,25 @@ public class AuthEndpoint {
         if(UserExists(email)) {
             return Response.status(401).build();
         } else {
-            // TODO: Query to add credentials
+            User user = new User();
+            user.email = email.toString();
+            user.password = pass.toString();
+            user.persist();
             return Response.status(200).build();
         }
     }
 
-    public Response ChangePass(String email, String oldPass, String newPass) {
+    public Response ChangePass(@QueryParam("email") String email, @QueryParam("password") String oldPass, String newPass) {
         if(oldPass.equals(getPass(email))) {
-            // TODO: Query to update the email's oldPass to newPass
+            // TODO: Query to update the user's oldPass to newPass
+            // Or to delete existing user and register the user again
+            return Response.status(200).build();
         } else {
             return Response.status(401).build();
         }
     }
 
-    public String getPass(String email) {
+    public String getPass(@QueryParam("email") String email) {
         String pass;
         // TODO: Query to pull the pass of the given email
 
