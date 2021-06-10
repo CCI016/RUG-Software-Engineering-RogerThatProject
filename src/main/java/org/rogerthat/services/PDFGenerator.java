164ede7b3f48af79d163993a2ecf8ca7d396af74
Gasan Rzaev <42190535+grzaev@users.noreply.github.com>
2021-06-10 21:15:34 +0200

    Commented structure added

--- src/main/java/org/rogerthat/services/PDFGenerator.java
+++ src/main/java/org/rogerthat/services/PDFGenerator.java
@@ -3,6 +3,7 @@ package org.rogerthat.services;
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.*;
 
+import javax.persistence.Table;
 import javax.swing.text.Document;
 import java.awt.*;
 import java.io.FileOutputStream;
@@ -11,7 +12,11 @@ import java.nio.file.Paths;
 
 public class PDFGenerator {
 
-    public void pdfGen() {
+    private void uploadPDF(Document document) {
+        // POST the generated document
+    }
+
+    public void generatePDF(Long personID) {
         // Adding RogerThat logo to the document
         Path path = Paths.get(ClassLoader.getSystemResource("roger_that_logo.png").toURI());
 
@@ -20,11 +25,21 @@ public class PDFGenerator {
         document.open();
         Image img = Image.getInstance(path.toAbsolutePath().toString());
         document.add(img);
-        // Add query to pull interval overview
-        // Make a PDFtable and add it to the document
+
+        // Query for person from Analyzer
+
+
+        // Create a table for monthly overview and add it to the document
+
+        // Depending on the amount of columns and their widths initialise the values
+        //float columnWidth[] = {};
+        //Table table = new Table(columnWidth);
+
+        // Do the following columnWidth.length times
+        // table.addCell(new Cell().add("Name of the column n").setBackgroundColor(Color.GRAY));
 
         document.close();
-        // Send document to the front end
+        uploadPDF(document);
     }
 
 }
